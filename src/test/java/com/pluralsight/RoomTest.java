797f7aedcc8e50cccd3374a591fce972fed5c637
David-Aldair <d.aldair.quinonez@gmail.com>
2025-10-22 14:23:49 -0400

    Implemented testing for checkIn, checkout, and cleanRoom.

---
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    //test for checkedIn()
+
+    @Test
+    public void occuppied_and_dirty_if_checkedIn(){
+
+        //arrange
+        Room checkedInRoom = new Room(2, 25, true, true);
+
+        //act
+        boolean isCheckedIn = checkedInRoom.isDirty();
+
+        boolean isCheckedIn2 = checkedInRoom.isOccupied();
+
+        //assert
+        assertTrue(isCheckedIn && isCheckedIn2);
+    }
+    //test for checkedOut
+    @Test
+    public void not_occuppied_but_is_dirty_if_checkedOut(){
+
+        //arrange
+        Room checkedOutRoom = new Room(2, 100, false, true);
+
+        //act
+        boolean isCheckedOut = checkedOutRoom.isOccupied();
+
+        boolean isCheckedOut2 = checkedOutRoom.isDirty();
+
+        //assert
+        assertFalse(isCheckedOut);
+        assertTrue(isCheckedOut2);
+    }
+    //test for cleanRoom
+    @Test
+    public void cleanRoom_if_notOcccupied_and_notDirty(){
+
+        //arrange
+        Room cleanedRoom = new Room(3, 100, false, false);
+
+        //act
+        boolean isClean = cleanedRoom.isDirty();
+
+        boolean isClean2 = cleanedRoom.isOccupied();
+
+        //assert
+        assertFalse(isClean && isClean2);
+    }
+
+}
